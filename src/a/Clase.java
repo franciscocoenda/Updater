43647980aa12acc66b7f568aaec1ab1503463ad9
Francisco Coenda <franciscocoenda@gmail.com>
2015-02-28 16:31:43 -0300

    l

--- src/a/Clase.java
+++ src/a/Clase.java
@@ -3,13 +3,14 @@ package a;
 public class Clase {
 	
 	private String cadena1 = "";
+	private String cadena2 = "Hola";
 
 	public void obtenerElementos (String elementos){
 		cadena1 = elementos;
 	}
 
 	private String calculador(){
-		return cadena1 + " Hola";
+		return cadena1 + cadena2;
 	}
 	
 }
